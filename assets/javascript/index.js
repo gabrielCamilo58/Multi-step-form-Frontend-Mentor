f2fe7ec232fd64e9e2df4a8729b49428b5356b4a
gabrielCamilo58 <gabrielaraujo2014.com@outlook.com>
2023-02-22 21:18:23 -0300

    :sparkles: go back

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,9 +2,16 @@
 function nextStep(e, next) {
     e.preventDefault();
    el = document.getElementById(`step-${next}`);
+   document.getElementById(`step-number-${next}`).classList.remove('selected');
    el.classList.add('hidden')
    el2 = document.getElementById(`step-${++next}`);
+   document.getElementById(`step-number-${next}`).classList.add('selected')
    el2.classList.remove('hidden')
-  console.log(el2)
-    console.log(next);
+}
+function backStep(e, number){
+  e.preventDefault();
+  document.getElementById(`step-${number}`).classList.add('hidden');
+  document.getElementById(`step-number-${number}`).classList.remove('selected');
+  document.getElementById(`step-${--number}`).classList.remove('hidden');
+  document.getElementById(`step-number-${number}`).classList.add('selected')
 }
